73241d56eab3d6969e298810fc909cb801ada99e
asomakia <asomakia@student.bridgew.edu>
2022-02-07 09:29:00 -0500

    Initial commit

---
+++ Temp.py
@@ -0,0 +1,6 @@
+def main():
+    rect_height = int((input("what is the height of the rectangle?")))
+    rect_width = int((input("what is the width of the rectangle?")))
+    print(f"the area of the rectangle is {rect_width*rect_height} cm")
+    print(f"the perimeter of the rectangle is {rect_width+rect_height+rect_height+rect_width} cm")
+main()
---
+++ main.py
@@ -0,0 +1,8 @@
+def print_hi(name):
+    your_name = input("what is your name")
+    print(f"hello{your_name}")
+    how_old = float(input("how old are you?"))
+    print(f"wow you are {how_old*7} in dog years")
+
+
+print_hi("ahmad")
